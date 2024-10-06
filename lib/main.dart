5ff1e4a10801d3ed125b3f1a3a1063a4769bea9c
elroymanuel944 <emcpara@gmail.com>
2024-10-06 17:14:29 +0530

    sad

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SplashScreen(),
+      home: SplashScreen(
+        title: 'Welcome to Clone App',
+      ),
     );
   }
 }
